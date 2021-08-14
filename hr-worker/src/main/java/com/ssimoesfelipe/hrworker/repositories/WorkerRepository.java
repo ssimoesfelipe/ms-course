22e706f05bd4abc4febf27d0e3f7e3f484047a62
FelipeSimoes1 <ssimoes.felipe@gmail.com>
2021-08-14 13:46:32 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/ssimoesfelipe/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.ssimoesfelipe.hrworker.repositories;
+
+import com.ssimoesfelipe.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
---
+++ hr-worker/src/test/java/com/ssimoesfelipe/hrworker/HrworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssimoesfelipe.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
